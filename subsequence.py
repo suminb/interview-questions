965e559a95c4c8c3bee3d9286a6d7d441c56af13
Sumin Byeon <suminb@gmail.com>
2018-06-25 00:13:26 +0900

    Subsequence problem

---
+++ subsequence.py
@@ -0,0 +1,33 @@
+"""
+Problem: Given a list of integers and a target number, find a consecutive
+subsequence that adds up to the target number.
+"""
+import pytest
+
+
+# TODO: Deal with negative integers
+def find(seq, target):
+    n = len(seq)
+    s = 0
+    i = j = 0
+
+    while True:
+        if s == target:
+            break
+        elif s < target and j < n:
+            s += seq[j]
+            j += 1
+        elif s > target and i < n:
+            s -= seq[i]
+            i += 1
+
+    return seq[i:j]
+
+
+@pytest.mark.parametrize('seq, target, expected', [
+    ([1, 2, 3, 4], -1, []),
+    ([1, 2, 3, 4], 1, [1]),
+    ([1, 2, 3, 4], 7, [3, 4]),
+])
+def test_find(seq, target, expected):
+    assert find(seq, target) == expected
